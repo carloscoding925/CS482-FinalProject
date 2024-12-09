c68ba5e05eb18e91bb5580ce8d39fb9ebade0718
Carlos Hernandez <carlosandresh925@gmail.com>
2024-12-08 18:03:31 -0800

    Added header

--- Web Client/app/components/headerComponent.tsx
+++ Web Client/app/components/headerComponent.tsx
@@ -1,9 +1,26 @@
 export default function HeaderComponent() {
     return (
         <div>
-            <header className="bg-cover bg-center h-48 text-white" style={{ backgroundImage: `url('/banner.jpeg')` }}>
-                <div className="flex flex-row">
-
+            <header className="bg-cover bg-center h-48 text-white flex flex-row" style={{ backgroundImage: `url('/banner.jpeg')` }}>
+                <div className="w-full flex flex-row justify-start">
+                    <div className="flex flex-col justify-end h-full">
+                        <h1 className="font-serif text-6xl">
+                            Weather Classification
+                        </h1>
+                    </div>
+                </div>
+                <div className="w-full flex flex-row justify-end">
+                    <div className="flex flex-row space-x-10">
+                        <h1 className="text-2xl flex flex-col justify-end">
+                            Home
+                        </h1>
+                        <h1 className="text-2xl flex flex-col justify-end">
+                            Models
+                        </h1>
+                        <h1 className="text-2xl flex flex-col justify-end pr-6">
+                            About
+                        </h1>
+                    </div>
                 </div>
             </header>
         </div>
