4bde9c409c7e8a6118de40f078e41215ddf05223
Carlos Hernandez <carlosandresh925@gmail.com>
2024-12-11 17:57:42 -0800

    built models home page

--- Web Client/app/routes/models+/_index.tsx
+++ Web Client/app/routes/models+/_index.tsx
@@ -3,16 +3,37 @@ import { Link } from "@remix-run/react";
 export default function Component() {
     return (
         <div>
+            <div className="h-20">
+
+            </div>
             <div className="flex flex-row place-content-center">
-                <div className="flex flex-col h-80 w-80" style={{ backgroundImage: `url('/logistic.jpeg')` }}>
+                <Link to="/models/logisticRegression">
+                    <div className="bg-cover flex flex-col justify-end h-80 w-80" style={{ backgroundImage: `url('/logistic.jpeg')` }}>
                     
-                </div>
-                <div className="flex flex-col h-80 w-80 mr-40 ml-40" style={{ backgroundImage: `url('/neuralnetwork.jpeg')` }}>
+                    </div>
+                    <h1 className="text-5xl font-bold hover:text-blue-600">
+                        Logistic Regression
+                    </h1>
+                </Link>
+                <Link to="/models/neuralNetwork">
+                    <div className="bg-cover flex flex-col justify-end h-80 w-80 mr-40 ml-40" style={{ backgroundImage: `url('/neuralnetwork.jpeg')` }}>
+                        
+                    </div>
+                    <h1 className="text-5xl font-bold mr-40 ml-40 hover:text-blue-600">
+                        Neural Network
+                    </h1>
+                </Link>
+                <Link to="/models/decisionTree">
+                    <div className="bg-cover flex flex-col justify-end h-80 w-80" style={{ backgroundImage: `url('/tree.jpeg')` }}>
+                        
+                    </div>
+                    <h1 className="text-5xl font-bold hover:text-blue-600">
+                        Decision Tree
+                    </h1>
+                </Link>
+            </div>
+            <div className="h-40">
 
-                </div>
-                <div className="flex flex-col h-80 w-80" style={{ backgroundImage: `url('/tree.jpeg')` }}>
-
-                </div>
             </div>
         </div>
     );
