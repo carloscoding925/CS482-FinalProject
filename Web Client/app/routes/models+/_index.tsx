d939464ffafdb0d16d0fe5b251f17ecdf0eebb22
Carlos Hernandez <carlosandresh925@gmail.com>
2024-12-11 17:43:09 -0800

    Added pictures for models

--- Web Client/app/routes/models+/_index.tsx
+++ Web Client/app/routes/models+/_index.tsx
@@ -3,9 +3,17 @@ import { Link } from "@remix-run/react";
 export default function Component() {
     return (
         <div>
-            <Link to={`../models/modelOne`}>
-                Link
-            </Link>
+            <div className="flex flex-row place-content-center">
+                <div className="flex flex-col h-80 w-80" style={{ backgroundImage: `url('/logistic.jpeg')` }}>
+                    
+                </div>
+                <div className="flex flex-col h-80 w-80 mr-40 ml-40" style={{ backgroundImage: `url('/neuralnetwork.jpeg')` }}>
+
+                </div>
+                <div className="flex flex-col h-80 w-80" style={{ backgroundImage: `url('/tree.jpeg')` }}>
+
+                </div>
+            </div>
         </div>
     );
 }
